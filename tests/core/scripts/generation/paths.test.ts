edd760d5c77da5169897ef18e9760562e5c9617e
Olivier Cartier <me@cestoliv.com>
2023-06-29 22:32:27 +0200

    style: 🎨 prettier

--- tests/core/scripts/generation/paths.test.ts
+++ tests/core/scripts/generation/paths.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { expect } from 'chai';
 import path from 'path';
 // import mock_fs from 'mock-fs';
@@ -12,7 +11,13 @@ import { set_conf } from '../../../../core/scripts/config'; // will use the fake
 vol.reset();
 // End create a fake config file for tests
 
-import { getFooterPath, getGeneratedPath, getHeaderPath, getThemePath, getWebPath } from '../../../../core/scripts/generation/paths';
+import {
+	getFooterPath,
+	getGeneratedPath,
+	getHeaderPath,
+	getThemePath,
+	getWebPath,
+} from '../../../../core/scripts/generation/paths';
 import { ESourceType } from '../../../../core/scripts/interfaces/interfaces';
 import { GENERATED_ROOT, SOURCE_ROOT, BUILTIN_THEMES_ROOT, CUSTOM_THEMES_ROOT } from '../../../../core/scripts/const';
 
@@ -22,44 +27,32 @@ describe('core/scripts/generation/paths.ts', () => {
 			const base_path = path.join(GENERATED_ROOT, 'content');
 
 			// Normal page
-			expect(
-				getGeneratedPath(path.join(SOURCE_ROOT, 'index.md'), ESourceType.Page)
-			).to.equal(
+			expect(getGeneratedPath(path.join(SOURCE_ROOT, 'index.md'), ESourceType.Page)).to.equal(
 				path.join(base_path, 'index.html'),
 			);
 
 			// Normal page in a 'source' subdirectory
-			expect(
-				getGeneratedPath(path.join(SOURCE_ROOT, 'source', 'index.markdown'), ESourceType.Page)
-			).to.equal(
+			expect(getGeneratedPath(path.join(SOURCE_ROOT, 'source', 'index.markdown'), ESourceType.Page)).to.equal(
 				path.join(base_path, 'source', 'index.html'),
 			);
 
 			// Normal image in a 'source' subdirectory in a 'source' subdirectory, as a page
 			expect(
-				getGeneratedPath(path.join(SOURCE_ROOT, 'source', 'source', 'index.png'), ESourceType.Page)
-			).to.equal(
-				path.join(base_path, 'source', 'source', 'index.html'),
-			);
+				getGeneratedPath(path.join(SOURCE_ROOT, 'source', 'source', 'index.png'), ESourceType.Page),
+			).to.equal(path.join(base_path, 'source', 'source', 'index.html'));
 
 			// Normal post in a 'blog/first_post' subdirectory
 			expect(
-				getGeneratedPath(path.join(SOURCE_ROOT, 'blog', 'first_post', 'post.md'), ESourceType.Post)
-			).to.equal(
-				path.join(base_path, 'blog', 'first_post', 'post.html'),
-			);
+				getGeneratedPath(path.join(SOURCE_ROOT, 'blog', 'first_post', 'post.md'), ESourceType.Post),
+			).to.equal(path.join(base_path, 'blog', 'first_post', 'post.html'));
 
 			// Normal podcast in a SOURCE_ROOT subdirectory
-			expect(
-				getGeneratedPath(path.join(SOURCE_ROOT, SOURCE_ROOT, 'episode.md'), ESourceType.Episode)
-			).to.equal(
+			expect(getGeneratedPath(path.join(SOURCE_ROOT, SOURCE_ROOT, 'episode.md'), ESourceType.Episode)).to.equal(
 				path.join(base_path, SOURCE_ROOT, 'episode.html'),
 			);
 
 			// Normal non-markdown file
-			expect(
-				getGeneratedPath(path.join(SOURCE_ROOT, 'imgs', 'image.webp'), ESourceType.Other)
-			).to.equal(
+			expect(getGeneratedPath(path.join(SOURCE_ROOT, 'imgs', 'image.webp'), ESourceType.Other)).to.equal(
 				path.join(base_path, 'imgs', 'image.webp'),
 			);
 		});
@@ -68,72 +61,48 @@ describe('core/scripts/generation/paths.ts', () => {
 	describe('getWebPath', () => {
 		it('should generate paths', () => {
 			// Normal page
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'index.html'), ESourceType.Page)
-			).to.equal(
-				'/',
-			);
+			expect(getWebPath(path.join(SOURCE_ROOT, 'index.html'), ESourceType.Page)).to.equal('/');
 
 			// Normal page in a 'test' subdirectory
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'test', 'index.html'), ESourceType.Page)
-			).to.equal(
+			expect(getWebPath(path.join(SOURCE_ROOT, 'test', 'index.html'), ESourceType.Page)).to.equal(
 				'/' + path.join('test'),
 			);
 
 			// Normal directory as a page
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'test'), ESourceType.Page)
-			).to.equal(
-				'/' + path.join('test'),
-			);
+			expect(getWebPath(path.join(SOURCE_ROOT, 'test'), ESourceType.Page)).to.equal('/' + path.join('test'));
 
 			// Normal page in a 'source' subdirectory
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'source', 'index.html'), ESourceType.Page)
-			).to.equal(
+			expect(getWebPath(path.join(SOURCE_ROOT, 'source', 'index.html'), ESourceType.Page)).to.equal(
 				'/' + path.join('source'),
 			);
 
 			// Normal image in a 'source' subdirectory in a 'source' subdirectory, as a page
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'source', 'source', 'image.png'), ESourceType.Page)
-			).to.equal(
+			expect(getWebPath(path.join(SOURCE_ROOT, 'source', 'source', 'image.png'), ESourceType.Page)).to.equal(
 				'/' + path.join('source', 'source', 'image'),
 			);
 
 			// Post in a 'blog' subdirectory
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'blog', 'first_post.md'), ESourceType.Post)
-			).to.equal(
+			expect(getWebPath(path.join(SOURCE_ROOT, 'blog', 'first_post.md'), ESourceType.Post)).to.equal(
 				'/' + path.join('blog', 'first_post'),
 			);
 
 			// Post in a 'blog/first_post' subdirectory
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'blog', 'first_post', 'post.md'), ESourceType.Post)
-			).to.equal(
+			expect(getWebPath(path.join(SOURCE_ROOT, 'blog', 'first_post', 'post.md'), ESourceType.Post)).to.equal(
 				'/' + path.join('blog', 'first_post'),
 			);
 
 			// Episode in a 'podcast' subdirectory
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'podcast', 'first.md'), ESourceType.Episode)
-			).to.equal(
+			expect(getWebPath(path.join(SOURCE_ROOT, 'podcast', 'first.md'), ESourceType.Episode)).to.equal(
 				'/' + path.join('podcast', 'first'),
 			);
 
 			// Episode in a 'podcast/first' subdirectory
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'podcast', 'first', 'episode.md'), ESourceType.Episode)
-			).to.equal(
+			expect(getWebPath(path.join(SOURCE_ROOT, 'podcast', 'first', 'episode.md'), ESourceType.Episode)).to.equal(
 				'/' + path.join('podcast', 'first'),
 			);
 
 			// Image
-			expect(
-				getWebPath(path.join(SOURCE_ROOT, 'imgs', 'image.webp'), ESourceType.Other)
-			).to.equal(
+			expect(getWebPath(path.join(SOURCE_ROOT, 'imgs', 'image.webp'), ESourceType.Other)).to.equal(
 				'/' + path.join('imgs', 'image.webp'),
 			);
 		});
@@ -142,61 +111,33 @@ describe('core/scripts/generation/paths.ts', () => {
 	describe('getThemePath', () => {
 		it('should generate paths', () => {
 			set_conf({});
-			expect(
-				getThemePath()
-			).to.equal(
-				path.join(BUILTIN_THEMES_ROOT, 'clean'),
-			);
+			expect(getThemePath()).to.equal(path.join(BUILTIN_THEMES_ROOT, 'clean'));
 
 			set_conf({ content: { theme: 'clean' } });
-			expect(
-				getThemePath()
-			).to.equal(
-				path.join(BUILTIN_THEMES_ROOT, 'clean'),
-			);
+			expect(getThemePath()).to.equal(path.join(BUILTIN_THEMES_ROOT, 'clean'));
 
 			set_conf({ content: { theme: 'test' } });
-			expect(
-				getThemePath()
-			).to.equal(
-				path.join(CUSTOM_THEMES_ROOT, 'test'),
-			);
+			expect(getThemePath()).to.equal(path.join(CUSTOM_THEMES_ROOT, 'test'));
 		});
 	});
 
 	describe('getHeaderPath', () => {
 		it('should generate paths', () => {
 			set_conf({});
-			expect(
-				getHeaderPath()
-			).to.equal(
-				null,
-			);
+			expect(getHeaderPath()).to.equal(null);
 
 			set_conf({ content: { header: 'file.md' } });
-			expect(
-				getHeaderPath()
-			).to.equal(
-				path.join(SOURCE_ROOT, 'file.md'),
-			);
+			expect(getHeaderPath()).to.equal(path.join(SOURCE_ROOT, 'file.md'));
 		});
 	});
 
 	describe('getFooterPath', () => {
 		it('should generate paths', () => {
 			set_conf({});
-			expect(
-				getFooterPath()
-			).to.equal(
-				null,
-			);
+			expect(getFooterPath()).to.equal(null);
 
 			set_conf({ content: { footer: 'file.md' } });
-			expect(
-				getFooterPath()
-			).to.equal(
-				path.join(SOURCE_ROOT, 'file.md'),
-			);
+			expect(getFooterPath()).to.equal(path.join(SOURCE_ROOT, 'file.md'));
 		});
 	});
 });
